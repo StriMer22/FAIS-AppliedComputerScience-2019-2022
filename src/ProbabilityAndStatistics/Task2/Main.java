2d04b6b6a0f16a9ee7ceeb2eed8fc362d741164b
StriMer22 <homen932@gmal.com>
2021-02-28 16:03:20 +0100

    ProbabilityAndsStatistics Java

---
+++ src/ProbabilityAndStatistics/Task2/Main.java
@@ -0,0 +1,7 @@
+package ProbabilityAndStatistics.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> new RandFrame().setVisible(true));
+    }
+}
---
+++ src/ProbabilityAndStatistics/Task3/Main.java
@@ -0,0 +1,14 @@
+package ProbabilityAndStatistics.Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        PoissonArrival poissonArrival = new PoissonArrival();
+        System.out.println("Wpisz lambde: ");
+        double mean = poissonArrival.N();
+        System.out.println("Wpisz N liczb: ");
+        int n = poissonArrival.N();
+        for (int i = 0; i < n; i++){
+            System.out.println(poissonArrival.poisson(mean));
+        }
+    }
+}
