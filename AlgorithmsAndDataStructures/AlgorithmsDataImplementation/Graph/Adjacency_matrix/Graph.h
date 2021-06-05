b2f4d8cdd12e643825f50f89867ed00abe754880
Dmytro Boiko <homen932@gmail.com>
2021-06-05 16:54:43 +0200

    Create Graph.h

---
+++ AlgorithmsAndDataStructures/AlgorithmsDataImplementation/Graph/Adjacency_matrix/Graph.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+class Graph {
+    bool **matrix;
+    int numberOfEdges;
+    int numberOfVertex;
+
+    bool checkVertex(int idx);
+
+public:
+    Graph();
+
+    void createVertices(int ile);
+
+    void addEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
+
+    void removeEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
+
+    bool checkEdge(int i_Vertex_Index_1, int i_Vertex_Index_2);
+
+    int vertexDegree(int idx);
+
+    list<int> getNeighbourIndices(int idx);
+
+    void printNeighbourIndices(int idx);
+
+    int getNumberOfEdges();
+
+    void readFromFile(std::string path);
+};
+
