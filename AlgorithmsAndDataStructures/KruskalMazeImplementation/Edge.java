809d7d5e7158d1219bc4f1437a1d2626031441d6
Dmytro Boiko <homen932@gmail.com>
2021-06-05 16:59:26 +0200

    Create Edge.java

---
+++ AlgorithmsAndDataStructures/KruskalMazeImplementation/Edge.java
@@ -0,0 +1,25 @@
+package KruskalMaze;
+
+class Edge {
+    private final int x;
+    private final int y;
+    private final int direction;
+
+    public Edge(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+}
