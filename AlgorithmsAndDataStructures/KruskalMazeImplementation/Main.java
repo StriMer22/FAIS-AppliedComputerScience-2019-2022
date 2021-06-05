729c3f78e39498e0ccb1e174841ff1f864062ac5
Dmytro Boiko <homen932@gmail.com>
2021-06-05 16:59:59 +0200

    Create Main.java

---
+++ AlgorithmsAndDataStructures/KruskalMazeImplementation/Main.java
@@ -0,0 +1,9 @@
+package KruskalMaze;
+
+import static java.awt.EventQueue.invokeLater;
+
+public class Main {
+    public static void main(String[] args) {
+        invokeLater(() -> new KruskalMaze().setVisible(true));
+    }
+}
